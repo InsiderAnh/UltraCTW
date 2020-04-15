4ea076dc30ecae32178455184f06680b66fbcb65
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 11:55:47 -0500

    Primer Commit

---
+++ src/main/java/io/github/Leonardo0013YT/UltraCTW/Main.java
@@ -0,0 +1,17 @@
+package io.github.Leonardo0013YT.UltraCTW;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
